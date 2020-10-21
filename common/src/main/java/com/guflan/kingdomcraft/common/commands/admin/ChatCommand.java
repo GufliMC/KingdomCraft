3244795eac1b1b5e3e12066aa1f9102eb1482f89
Joris Guffens <joris.guffens@outlook.com>
2020-10-21 18:25:46 +0200

    added /k chat command

---
+++ common/src/main/java/com/guflan/kingdomcraft/common/commands/admin/ChatCommand.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of KingdomCraft.
+ *
+ * KingdomCraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KingdomCraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KingdomCraft. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.guflan.kingdomcraft.common.commands.admin;
+
+import com.guflan.kingdomcraft.api.chat.ChatChannel;
+import com.guflan.kingdomcraft.api.domain.Kingdom;
+import com.guflan.kingdomcraft.api.domain.User;
+import com.guflan.kingdomcraft.api.entity.PlatformLocation;
+import com.guflan.kingdomcraft.api.entity.PlatformPlayer;
+import com.guflan.kingdomcraft.api.entity.PlatformSender;
+import com.guflan.kingdomcraft.common.KingdomCraftImpl;
+import com.guflan.kingdomcraft.common.command.CommandBaseImpl;
+
+public class ChatCommand extends CommandBaseImpl {
+
+    public ChatCommand(KingdomCraftImpl kdc) {
+        super(kdc, "chat", -1, true);
+        setExplanationMessage(kdc.getMessageManager().getMessage("cmdChatExplanation"));
+        setPermissions("kingdom.chat");
+    }
+
+    @Override
+    public void execute(PlatformSender sender, String[] args) {
+        PlatformPlayer player = (PlatformPlayer) sender;
+
+        ChatChannel channel = kdc.getChatManager().getChatChannel(args[0]);
+        if ( channel == null ) {
+            kdc.getMessageManager().send(sender, "cmdChatNoChannel", args[0]);
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for ( int i = 1; i < args.length; i++ ) {
+            sb.append(args[i]);
+        }
+
+        String msg = sb.toString().trim();
+        if ( msg.equals("") ) {
+            kdc.getMessageManager().send(sender, "cmdChatEmptyMessage");
+            return;
+        }
+
+        kdc.getChatDispatcher().send(player, channel, msg);
+    }
+}
